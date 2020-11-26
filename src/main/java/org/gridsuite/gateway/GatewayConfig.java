073737c3989f919603dd8932c771bb4a19f1e236
AbdelHedhili <abdelsalem.hedhili@rte-france.com>
2020-11-26 18:31:07 +0100

    add config-ui and config-ui-notif
    Signed-off-by: AbdelHedhili <abdelsalem.hedhili@rte-france.com>

--- src/main/java/org/gridsuite/gateway/GatewayConfig.java
+++ src/main/java/org/gridsuite/gateway/GatewayConfig.java
@@ -56,6 +56,17 @@ public class GatewayConfig {
                     .filters(f -> f.rewritePath("/actions/(.*)", "/$1"))
                     .uri(uriConfiguration.getActionsServerBaseUri())
             )
+            .route(p -> p
+                    .path("/config-ui/**")
+                    .filters(f -> f.rewritePath("/config-ui/(.*)", "/$1"))
+                    .uri(uriConfiguration.getConfigUiServerBaseUri())
+            )
+            .route(p -> p
+                    .path("/config-notification/**")
+                    .filters(f -> f.rewritePath("/config-notification/(.*)", "/$1"))
+                    .uri(uriConfiguration.getConfigNotificationServerBaseUri())
+            )
+
             .build();
     }
 }
@@ -68,6 +79,8 @@ class UriConfiguration {
     @Value("${backing-services.notification-server.base-uri:http://notification-server/}") String notificationServerBaseUri;
     @Value("${backing-services.merge-notification-server.base-uri:http://merge-notification-server/}") String mergeNotificationServerBaseUri;
     @Value("${backing-services.actions-server.base-uri:http://actions-server/}") String actionsServerBaseUri;
+    @Value("${backing-services.config-ui-server.base-uri:http://config-ui-server/}") String configUiServerBaseUri;
+    @Value("${backing-services.config-notification-server.base-uri:http://config-notification-server/}") String configNotificationServerBaseUri;
 
     public String getCaseServerBaseUri() {
         return caseServerBaseUri;
@@ -115,5 +128,21 @@ class UriConfiguration {
 
     public void setActionsServerBaseUri(String actionsServerBaseUri) {
         this.actionsServerBaseUri = actionsServerBaseUri;
+    }
+
+    public String getConfigUiServerBaseUri() {
+        return configUiServerBaseUri;
+    }
+
+    public void setConfigUiServerBaseUri(String configUiServerBaseUri) {
+        this.configUiServerBaseUri = configUiServerBaseUri;
+    }
+
+    public String getConfigNotificationServerBaseUri() {
+        return configNotificationServerBaseUri;
+    }
+
+    public void setConfigNotificationServerBaseUri(String configNotificationServerBaseUri) {
+        this.configNotificationServerBaseUri = configNotificationServerBaseUri;
     }
 }
