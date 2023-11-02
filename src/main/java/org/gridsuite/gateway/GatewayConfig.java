e6a8460e37f0ce4cf6c6095fb1a349d3484d93f0
Florent MILLOT <75525996+flomillot@users.noreply.github.com>
2023-11-02 10:29:59 +0100

    Add shortcircuit to the list of servers (#91)
    Signed-off-by: Florent MILLOT <millotflo@gmail.com>

--- src/main/java/org/gridsuite/gateway/GatewayConfig.java
+++ src/main/java/org/gridsuite/gateway/GatewayConfig.java
@@ -59,6 +59,7 @@ public class GatewayConfig {
             .route(p -> context.getBean(DynamicSimulationServer.class).getRoute(p))
             .route(p -> context.getBean(CaseImportServer.class).getRoute(p))
             .route(p -> context.getBean(VoltageInitServer.class).getRoute(p))
+            .route(p -> context.getBean(ShortCircuitServer.class).getRoute(p))
             .build();
     }
 }
