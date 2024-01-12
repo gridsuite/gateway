d13214d89d3ae4e1767814354c637a68b527ec26
FranckLecuyer <47824306+FranckLecuyer@users.noreply.github.com>
2024-01-12 16:31:05 +0100

    default provider for sensitivity analysis non evacuated energy (#93)
    * default provider for non evacuated energy
    
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/test/java/org/gridsuite/gateway/ElementAccessControlTest.java
+++ src/test/java/org/gridsuite/gateway/ElementAccessControlTest.java
@@ -130,9 +130,9 @@ public class ElementAccessControlTest {
         stubFor(get(urlEqualTo("/v1/svg-component-libraries")).withHeader("userId", equalTo("user1")).willReturn(aResponse()));
         stubFor(get(urlEqualTo("/v1/export-network-formats")).withHeader("userId", equalTo("user1")).willReturn(aResponse()));
         stubFor(get(urlEqualTo("/v1/loadflow-default-provider")).withHeader("userId", equalTo("user1")).willReturn(aResponse()));
-        stubFor(get(urlEqualTo("/v1/loadflow-default-provider")).withHeader("userId", equalTo("user1")).willReturn(aResponse()));
         stubFor(get(urlEqualTo("/v1/security-analysis-default-provider")).withHeader("userId", equalTo("user1")).willReturn(aResponse()));
         stubFor(get(urlEqualTo("/v1/sensitivity-analysis-default-provider")).withHeader("userId", equalTo("user1")).willReturn(aResponse()));
+        stubFor(get(urlEqualTo("/v1/non-evacuated-energy-default-provider")).withHeader("userId", equalTo("user1")).willReturn(aResponse()));
         stubFor(get(urlEqualTo("/v1/dynamic-simulation-default-provider")).withHeader("userId", equalTo("user1")).willReturn(aResponse()));
         webClient
             .get().uri("study/v1/search")
@@ -164,6 +164,11 @@ public class ElementAccessControlTest {
                 .header("Authorization", "Bearer " + tokenUser1)
                 .exchange()
                 .expectStatus().isOk();
+        webClient
+            .get().uri("study/v1/non-evacuated-energy-default-provider")
+            .header("Authorization", "Bearer " + tokenUser1)
+            .exchange()
+            .expectStatus().isOk();
         webClient
                 .get().uri("study/v1/dynamic-simulation-default-provider")
                 .header("Authorization", "Bearer " + tokenUser1)
