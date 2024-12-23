ced8f67aef6dae207666f8b4691c8c9872a7987a
Seddik Yengui <seddik.yengui_externe@rte-france.com>
2024-12-23 17:10:22 +0100

    review fix
    Signed-off-by: Seddik Yengui <seddik.yengui_externe@rte-france.com>

--- src/main/java/org/gridsuite/gateway/NettyMetricsConfiguration.java
+++ src/main/java/org/gridsuite/gateway/NettyMetricsConfiguration.java
@@ -7,18 +7,38 @@
 
 package org.gridsuite.gateway;
 
+import io.micrometer.core.instrument.config.MeterFilter;
 import org.springframework.boot.web.embedded.netty.NettyServerCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.netty.http.server.HttpServer;
 
 import java.util.function.Function;
 
-// As discussed here https://stackoverflow.com/questions/66028195/spring-webflux-actuator-netty-thread-metrics,
-// the metrics expose by reactor-netty are not enabled by default in spring boot. To enable them we add the following bean
+/**
+ * @author Seddik Yengui <seddik.yengui_externe at rte-france.com>
+ */
+
+// the metrics expose by reactor-netty are not enabled by default in spring boot.
+// To enable them we add the following bean to customise the Netty HttpServer.
 @Configuration
 public class NettyMetricsConfiguration implements NettyServerCustomizer {
     @Override
     public HttpServer apply(HttpServer httpServer) {
         return httpServer.metrics(true, Function.identity());
+    }
+
+    @Bean
+    public MeterFilter meterFilter() {
+        return MeterFilter.denyUnless(id -> {
+            String name = id.getName();
+            // Allow all non-reactor metrics
+            if (!name.startsWith("reactor.netty")) {
+                return true;
+            }
+            // Allow only the specific reactor metrics that we use
+            return name.equals("reactor.netty.http.server.connections") ||
+                    name.equals("reactor.netty.http.server.connections.active");
+        });
     }
 }
