be21e707c4abd0d51d4baf9a2cd45653bc65fea2
SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>
2025-12-16 14:39:20 +0100

    fix
    Signed-off-by: SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>

--- src/main/java/org/gridsuite/gateway/endpoints/PccMinServer.java
+++ src/main/java/org/gridsuite/gateway/endpoints/PccMinServer.java
@@ -25,7 +25,7 @@ public class PccMinServer implements EndPointServer {
 
     @Override
     public String getEndpointBaseUri() {
-        return servicesURIsConfig.getGeoDataServerBaseUri();
+        return servicesURIsConfig.getPccMinServerBaseUri();
     }
 
     @Override
