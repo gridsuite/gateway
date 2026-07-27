2f2b1ac49ef84f46208b76fe00e51a0b1f4a1c1c
Radouane Khouadri <redouane.khouadri_externe@rte-france.com>
2026-07-27 14:04:32 +0200

    add an error log when accessing restricted backend service

--- src/main/java/org/gridsuite/gateway/filters/LoggingFilter.java
+++ src/main/java/org/gridsuite/gateway/filters/LoggingFilter.java
@@ -10,11 +10,16 @@ import org.gridsuite.gateway.services.UserAdminService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
 
 /**
  * @author Radouane KHOUADRI <redouane.khouadi_externe at rte-france.com>
@@ -51,23 +56,33 @@ public class LoggingFilter extends AbstractGlobalPreFilter {
      */
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        String requestPath = exchange.getRequest().getPath().toString();
 
-        boolean isAllowedPath = Arrays.stream(ALLOWED_PATHS)
-                .anyMatch(requestPath::startsWith);
+        Set<URI> originalUris = exchange.getAttributeOrDefault(
+                ServerWebExchangeUtils.GATEWAY_ORIGINAL_REQUEST_URL_ATTR,
+                Collections.emptySet()
+        );
 
-        if (!isAllowedPath) {
-            LOGGER.error("Unauthorized access to restricted backend service. path='{}', method='{}', clientIp='{}'.",
-                    requestPath,
-                    exchange.getRequest().getMethod(),
-                    exchange.getRequest().getRemoteAddress());
-        }
+        originalUris.stream().findFirst().ifPresent(originalUri -> {
+            String originalPath = originalUri.getPath();
+
+            boolean isAllowedPath = Arrays.stream(ALLOWED_PATHS)
+                    .anyMatch(originalPath::startsWith);
+
+            if (!isAllowedPath) {
+                LOGGER.error(
+                        "Unauthorized access to restricted backend service. path='{}', method='{}', clientIp='{}'.",
+                        originalPath,
+                        exchange.getRequest().getMethod(),
+                        exchange.getRequest().getRemoteAddress()
+                );
+            }
+        });
 
         return chain.filter(exchange);
     }
 
     @Override
     public int getOrder() {
-        return Ordered.HIGHEST_PRECEDENCE;
+        return Ordered.LOWEST_PRECEDENCE - 1;
     }
 }
