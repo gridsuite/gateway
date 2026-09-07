3df1f924b4562af02b46831ec00036650acd747c
HARPER Jon <jon.harper87@gmail.com>
2026-09-07 17:11:12 +0200

    Fix memory leak because of netty high cardinality URI on metrics
    The heap fills up until OOM in days/weeks with these because they
    they have the full uri (e.g. containing an uuid) as a tag.
    
    meter$id name | instances count |
    ---------------------------------------------------------
    reactor.netty.http.server.data.sent.time | 71 742 |
    reactor.netty.http.server.response.time | 71 742 |
    reactor.netty.http.server.data.received.time| 68 043 |
    reactor.netty.http.server.data.received | 42 998 |
    reactor.netty.http.server.data.sent | 42 998
    
    They are not visible in prometheus because they are filtered by a spring
    meterfilter for the spring managed meter registry, but they are still unable to
    be GC'd because they are also stored in micrometer's static default
    globalRegistry (that's where reactor-netty puts them; the spring managed
    registry is setup by spring as a child of the default registry to receive these
    metrics)

--- src/main/java/org/gridsuite/gateway/NettyMetricsConfiguration.java
+++ src/main/java/org/gridsuite/gateway/NettyMetricsConfiguration.java
@@ -7,9 +7,10 @@
 
 package org.gridsuite.gateway;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.config.MeterFilter;
 import org.springframework.boot.web.embedded.netty.NettyServerCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.netty.http.server.HttpServer;
 
@@ -20,13 +21,15 @@ import java.util.function.Function;
  * @author Seddik Yengui <seddik.yengui_externe at rte-france.com>
  */
 
-// Enable Netty metrics that are not enabled by default in Spring Boot.
+// Enable safe and useful Netty metrics that are not enabled by default in Spring Boot.
 @Configuration
 public class NettyMetricsConfiguration implements NettyServerCustomizer {
 
     private static final String REACTOR_NETTY_PREFIX = "reactor.netty";
-    // If additional metrics are added, ensure the URIs are provided in a template-like format.
-    // Without this, each unique URI generates a separate tag, which takes a lot of memory
+    // If additional metrics are added, ensure they don't have uri as a tag because we
+    // don't map reactor-netty uris to a low cardinality space like fixed uri templates.
+    // Otherwise we will get OOMs.
+    // see httpServer.metrics() configuration below.
     private static final List<String> ALLOWED_REACTOR_NETTY_METRICS = List.of(
             "reactor.netty.http.server.connections.total",
             "reactor.netty.http.server.connections.active"
@@ -34,19 +37,32 @@ public class NettyMetricsConfiguration implements NettyServerCustomizer {
 
     @Override
     public HttpServer apply(HttpServer httpServer) {
+        // - The filter is registered here before metrics are enabled to easily
+        //   guarantee it's active at the time metrics are produced.
+        // - Reactor Netty documents that it registers its meters in Micrometer's
+        //   static Metrics.globalRegistry so the filter must be set on the
+        //   global registry itself (not the registry setup by spring-boot) otherwise
+        //   we get OOM errors
+        denyHighCardinalityReactorNettyMetrics(Metrics.globalRegistry);
+
+        // NOTE: here passing Function.identity() as the second argument to
+        // httpServer.metrics(), we don't normalize/unify uris which is mandatory
+        // to use the metrics that add uri as a tag (otherwise this will cause
+        // OOMs), hence the current need to filter and keep only metrics without uri.
+        // The reason is that the gateway reverse proxies everything without
+        // having have a list of endpoints (= uri templates) to map to.
         return httpServer.metrics(true, Function.identity());
     }
 
-    @Bean
-    public MeterFilter meterFilter() {
-        return MeterFilter.denyUnless(id -> {
+    private static void denyHighCardinalityReactorNettyMetrics(MeterRegistry meterRegistry) {
+        meterRegistry.config().meterFilter(MeterFilter.denyUnless(id -> {
             String name = id.getName();
-            // Allow all non-reactor metrics
+            // Don't interfere with other non reactor-netty metrics
             if (!name.startsWith(REACTOR_NETTY_PREFIX)) {
                 return true;
             }
             // Allow only the specific reactor metrics that we use
             return ALLOWED_REACTOR_NETTY_METRICS.contains(name);
-        });
+        }));
     }
 }
