97a7084f3a636a9de89447592d9a489233eb3b76
HARPER Jon <jon.harper87@gmail.com>
2026-09-12 09:52:19 +0200

    Remove more access control unused code (continuing #113, #107)

--- src/main/java/org/gridsuite/gateway/endpoints/EndPointElementServer.java
+++ src/main/java/org/gridsuite/gateway/endpoints/EndPointElementServer.java
@@ -6,24 +6,8 @@
  */
 package org.gridsuite.gateway.endpoints;
 
-import org.springframework.http.HttpMethod;
-
-import java.util.Set;
-
-import static org.springframework.http.HttpMethod.*;
-
 /**
  * @author Slimane Amar <slimane.amar at rte-france.com>
  */
 public interface EndPointElementServer extends EndPointServer {
-
-    String QUERY_PARAM_IDS = "ids";
-
-    Set<HttpMethod> ALLOWED_HTTP_METHODS = Set.of(GET, HEAD,
-            PUT, POST, DELETE
-    );
-
-    default Set<String> getUncontrolledRootPaths() {
-        return Set.of();
-    }
 }
